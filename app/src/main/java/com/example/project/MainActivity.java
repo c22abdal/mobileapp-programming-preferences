c862c867022f27b9a3aa241a709f580db908f9de
Abdalrahman Alhindi <71189461+Samurai-Ebben@users.noreply.github.com>
2024-05-02 20:08:05 +0200

    Creating SecondActivity

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,18 +1,44 @@
 package com.example.project;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button openPage;
+    TextView sharedTxt;
+    private SharedPreferences sharedPreferences;
+    private SharedPreferences.Editor prefEditor;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        openPage = findViewById(R.id.openBtn);
+
+        sharedPreferences = getPreferences(MODE_PRIVATE);
+        prefEditor = sharedPreferences.edit();
+
+
+        sharedTxt = findViewById(R.id.textView);
+        openPage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 }
