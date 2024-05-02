5fa2dae04b86065a97fe66b51c193d949f496304
Abdalrahman Alhindi <71189461+Samurai-Ebben@users.noreply.github.com>
2024-05-02 20:27:35 +0200

    Saving data with a click of a button

--- app/src/main/java/com/example/project/SecondActivity.java
+++ app/src/main/java/com/example/project/SecondActivity.java
@@ -2,6 +2,7 @@ package com.example.project;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -24,5 +25,14 @@ public class SecondActivity extends AppCompatActivity {
         sharedPreferences = getPreferences(MODE_PRIVATE);
         prefEditor = sharedPreferences.edit();
         setContentView(R.layout.activity_second);
+        saveBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                prefEditor.putString("Data", editTxt.getText().toString());
+                prefEditor.apply();
+
+                finish();
+            }
+        });
     }
 }
